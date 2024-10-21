4fb785d5b8e986990e1b074cbb9e547255b8b4f1
gopi263preethi <gopi.itsuppport305@gmail.com>
2024-10-21 15:30:53 +0000

    python

---
+++ manage.py
@@ -0,0 +1,2 @@
+python is versatile and easy to learn language and it is extensively used in web design and mobile apps and desktop apps and more extensively used in Data Science amndmachine learning
+
